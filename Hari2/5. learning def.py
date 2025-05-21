70a70053284193f9a84d37ab973a1de37a8f6753
Windu Putra <windups21@gmail.com>
2025-05-21 12:26:03 +0700

    Making Fun With Game

--- Hari2/5. learning def.py
+++ Hari2/5. learning def.py
@@ -1,14 +1,13 @@
-def buat_bakso(jenis, ukuran):
-    if jenis == "kecil":
-        harga = 1000
-    else:
-        harga = 5000
-
-    if ukuran:
-        return f"Bakso {jenis} ukuran {ukuran} dengan harga {harga}"
-    else:
-        return f"Bakso {jenis} dengan harga {harga}"
-
-print(buat_bakso("kecil", "besar"))
-
+def cek_angka():
+	try:
+		angka = int(input("Masukkan Angka: "))
+		if angka <= 1:
+			print(f'Angka {angka} Bukan Bilangan Prima')
+		elif angka % 2 != 0 and angka % 3 != 0 and angka % 5 != 0 and angka % 7 != 0:
+			print(f"Angka {angka} Bukan Bilangan Prima")
+		else:
+			print(f"Bilangan {angka} adalah angka Prima")
+	except ValueError:
+		print("Angka Invalid")
+cek_angka()
 
